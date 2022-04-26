20c59f06c5c37f28f633965cfe9e515f495f2ecd
mudi-igbinoba <ruth.igbinoba12@gmail.com>
2022-04-26 17:57:45 +0100

    feat: Created table grid from user input sizes
    Successfully created the necessary variables for table, form input,
    color picker. Also worked on the makeGrid function and was able to make
    a table grid with the input values the user provides.

--- designs.js
+++ designs.js
@@ -1,10 +1,46 @@
 // Select color input
-// Select size input
+const colorInput = document.querySelector('#colorPicker')
+
+// To get the color value of colorInput
+colorInput.value
+
+// Select size input - height, width
+const heightInput = document.querySelector('#inputHeight')
+const widthInput = document.querySelector('#inputWidth')
+
+// Getting the form
+const form = document.querySelector('#sizePicker')
+form.addEventListener('submit', makeGrid)
+
+//Getting the table
+const table = document.querySelector('#pixelCanvas')
+console.log(table)
+
+//Creating thead, tbody and tr
+const tableBody = document.createElement('tbody')
+table.appendChild(tableBody);
+
+
+
+
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(e) {
+// Prevent reloading
+    e.preventDefault()
 
-function makeGrid() {
+//Changing gridweight and gridheight values to number
+    let heightValue = Number(heightInput.value)
+    let widthValue = Number(widthInput.value)
 
-// Your code goes here!
+// for creating the number of rows
+    for(var i = 0; i < heightValue; i++){
+        const tableRow = document.createElement('tr')
+        tableBody.appendChild(tableRow)
 
+        for(var j = 0; j < widthValue; j++){
+            const tableColumn = document.createElement('td')
+            tableRow.appendChild(tableColumn)
+        }
+    }
 }
